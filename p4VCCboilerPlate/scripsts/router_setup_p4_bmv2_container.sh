4eeb8906daea82a0b1c064be829c7ec93342f9a3
Durbek-Gafur <durbek.gafurov@gmail.com>
2022-09-01 10:33:51 -0500

    boiler plate

--- p4VCCboilerPlate/scripsts/router_setup_p4_bmv2_container.sh
+++ p4VCCboilerPlate/scripsts/router_setup_p4_bmv2_container.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 
+port1_iface=$1
+port2_iface=$2
+port3_iface=$3
 
-ip_type=$1
-ifaces="${@:2}"
 
 echo Hello, FABRIC. From node `hostname -s`
 sudo apt-get update
 sudo apt-get install -y docker.io
 
-if [ $ip_type = 'IPv4' ]
-then
-  docker run -d -it --cap-add=NET_ADMIN --privileged --name fabric_p4 registry.ipv4.docker.com/pruth/fabric-images:0.0.2j
-else
-  docker run -d -it --cap-add=NET_ADMIN --privileged --name fabric_p4 registry.ipv6.docker.com/pruth/fabric-images:0.0.2j
-  sed -i '/nameserver/d' /etc/resolv.conf 
-  echo nameserver 2a00:1098:2c::1 >> /etc/resolv.conf 
-  echo nameserver 2a01:4f8:c2c:123f::1 >> /etc/resolv.conf 
-  echo nameserver 2a00:1098:2b::1 >> /etc/resolv.conf 
-fi
+docker run -d -it --cap-add=NET_ADMIN --privileged --name fabric_p4 registry.ipv4.docker.com/pruth/fabric-images:0.0.2j
 
 sleep 10
 NSPID=$(docker inspect --format='{{ .State.Pid }}' fabric_p4)
 echo NSPID $NSPID
 
 switch_interface_args=""
-swtich_port=0
-for iface in $ifaces; do
+swtich_port=1
+for iface in $@; do
   ip link set dev $iface promisc on
   ip link set $iface netns $NSPID
   docker exec fabric_p4 ip link set dev $iface up
@@ -36,11 +28,11 @@ for iface in $ifaces; do
   swtich_port=$((swtich_port+1))
 done
 
-
 echo Starting switch
 docker exec -w /root/tutorials/exercises/basic_tunnel fabric_p4 sh -c 'cp basic_tunnel.p4 basic_tunnel.working.p4'
 docker exec -w /root/tutorials/exercises/basic_tunnel fabric_p4 sh -c 'cp solution/basic_tunnel.p4 basic_tunnel.p4'
 docker exec -w /root/tutorials/exercises/basic_tunnel fabric_p4 sh -c 'p4c --p4runtime-files basic_tunnel.txt --target bmv2 --arch v1model basic_tunnel.p4'
-docker exec -d -it fabric_p4 sh -c 'simple_switch '${switch_interface_args}' /root/tutorials/exercises/basic_tunnel/basic_tunnel.json'
+docker exec -d -it fabric_p4 sh -c 'simple_switch --interface 1@'${port1_iface}' --interface 2@'${port2_iface}' --interface 3@'${port3_iface}' /root/tutorials/exercises/basic_tunnel/basic_tunnel.json'
+echo 'simple_switch '${switch_interface_args}' /root/tutorials/exercises/basic_tunnel/basic_tunnel.json'
 
 echo done!
